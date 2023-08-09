334606bfcb9f9a3f07be1081d8449968195163ce
estolz <estolz@smail.uni-koeln.de>
2023-08-09 21:05:50 +0200

    updated static root again

--- VVV/vvv/settings.py
+++ VVV/vvv/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = "/home/myusername/myproject/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "static/"
 
