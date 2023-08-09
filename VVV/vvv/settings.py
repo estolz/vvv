45c40b21df21001c70fc5343a421c47cf882e0d4
estolz <estolz@smail.uni-koeln.de>
2023-08-09 21:00:29 +0200

    updated sattic root

--- VVV/vvv/settings.py
+++ VVV/vvv/settings.py
@@ -100,6 +100,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = "/home/myusername/myproject/static"
+
 STATIC_URL = "static/"
 
 # Default primary key field type
